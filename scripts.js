9f3201607717f974437f8471a4337a3214db00a2
Carlos Ramos <carlos.dg147@gmail.com>
2023-01-21 23:49:10 -0400

    formulario register - html y css.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
-var buttons = document.querySelectorAll(".button");
+(function() {
 
-for (const btn of buttons) {
-    btn.addEventListener('click', function(e) {
-        e.preventDefault();
-        for (const btnOthers of buttons) {
-            btnOthers.classList.remove("active");
-        }
-        btn.classList.add("active");
-
-    })
-}
+    var buttons = document.querySelectorAll(".button");
+    
+    for (const btn of buttons) {
+        btn.addEventListener('click', function(e) {
+            e.preventDefault();
+            for (const btnOthers of buttons) {
+                btnOthers.classList.remove("active");
+            }
+            btn.classList.add("active");
+    
+        })
+    }
+})();
