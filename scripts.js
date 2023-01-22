7e9c9fb198d3deafed8a68d5f062eea674ff6aaa
Carlos Ramos <carlos.dg147@gmail.com>
2023-01-22 00:35:00 -0400

    transition en los formularios y javascript.

--- scripts.js
+++ scripts.js
@@ -12,4 +12,32 @@
     
         })
     }
+
+    var containerForm = document.getElementById('containerForm');
+
+    var btnSignin = document.getElementById("btnSignin");
+    btnSignin.addEventListener('click', function(e){
+        e.preventDefault();
+        containerForm.classList.add("signin");
+        containerForm.classList.remove("register-1");
+        containerForm.classList.remove("register-2");
+    });
+
+    var btnReg = document.getElementById("btnReg");
+    btnReg.addEventListener('click', function(e){
+        e.preventDefault();
+        containerForm.classList.add("register-1");
+        containerForm.classList.remove("register-2");
+        containerForm.classList.remove("signin")
+
+    });
+
+    var btnNext = document.getElementById("btnNext");
+    btnNext.addEventListener('click', function(e){
+        e.preventDefault();
+        containerForm.classList.add("register-2");
+        containerForm.classList.remove("register-1")
+        containerForm.classList.remove("signin")
+    });
+    
 })();
